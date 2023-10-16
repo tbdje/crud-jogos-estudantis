063257e0c9a2615684120007987d75c52b3e9cba
tbdje <146776483+tbdje@users.noreply.github.com>
2023-10-16 13:05:31 +0000

    adicao controllers

--- src/controller/controller_jogo.py
+++ src/controller/controller_jogo.py
@@ -19,36 +19,97 @@ class ControllerJogo:
 
         if escola is None:
             return None
-        
+       
         data_jogo = input("Insira a data que ocorrerá o jogo (dia/mes/ano): ").strip()
         hora_jogo = input("Insira a hora em que o jogo acontecerá (hora:minuto): ").strip()
 
         dia, mes, ano = list(map(int, data_jogo.split("/")))
         hora, minuto = list(map(int, hora_jogo.split(":")))
-        
+       
         datahora = datetime(ano, mes, dia, hora, minuto).strftime("%Y-%m-%d %H:%M")
 
         oracle = OracleQueries(can_write=True)
         cursor = oracle.connect()
         tipo_dado_codigo = cursor.var(int)
 
-        mapeamento_jogo = dict(id_jogo=tipo_dado_codigo, datahora=datahora, escola=escola.get_cnpj())
+        mapeamento_jogo = dict(id_jogo=tipo_dado_codigo, datahora=datahora, cnpj=escola.get_cnpj())
 
-        cursor.execute("""
+        cursor.execute(f"""
         BEGIN
-            :ID_JOGO := JOGOS_ID_SEQ.NEXTVAL;
-            INSERT  INTO JOGOS VALUES (:ID_JOGO, :DATA_HORA, :CNPJ)
+            :id_jogo := JOGOS_ID_SEQ.NEXTVAL;
+            INSERT INTO JOGOS VALUES(:id_jogo, TO_TIMESTAMP(:datahora, 'YYYY-MM-DD HH24:MI'), :cnpj);
         END;
         """, mapeamento_jogo)
 
-        id_jogo = tipo_dado_codigo.getvalue()
+        id_do_jogo = tipo_dado_codigo.getvalue()
         oracle.conn.commit()
 
-    def atualizar_registro(self):
-        pass
+        registro_jogo = json.loads(oracle.sqlToJson(f"SELECT DATA_HORA FROM JOGOS WHERE ID_JOGO = {id_do_jogo}"))[0]
+        jogo = Jogo(id_do_jogo, registro_jogo["data_hora"], escola)
 
-    def deletar_registro(self):
-        pass
+        print(jogo.to_string())
+        return jogo
+
+    def atualizar_registro(self) -> Union[Jogo, None]:
+        self.listar_jogos()
+        id_jogo = int(input("Insira o ID do jogo que deseja alterar: ").strip())
+
+        if self.verificar_registro(id_jogo):            
+            data_jogo = input("Insira a nova data do jogo (dia/mes/ano): ").strip()
+            hora_jogo = input("Insira o horário: (hora:minuto): ").strip()
+
+            dia, mes, ano = list(map(int, data_jogo.split("/")))
+            hora, minuto = list(map(int, hora_jogo.split(":")))
+
+            datahora = datetime(ano, mes, dia, hora, minuto).strftime("%Y-%m-%d %H:%M")
+
+            oracle = OracleQueries(can_write=True)
+            oracle.connect()
+
+            oracle.write(f"UPDATE JOGOS SET DATA_HORA = TO_TIMESTAMP('{datahora}', 'YYYY-MM-DD HH24:MI') WHERE ID_JOGO = {id_jogo}")
+
+        else:
+            print("[!] Esse jogo não existe no sistema.")
+            return None
+       
+
+    def deletar_registro(self) -> Union[Jogo, None]:
+        id_jogo = int(input("Insira o ID do jogo que deseja deletar: "))
+
+        oracle = OracleQueries(can_write=True)
+        oracle.connect()
+
+        if self.verificar_registro(id_jogo):
+            registro_jogo = json.loads(oracle.sqlToJson(f"SELECT * FROM JOGOS WHERE ID_JOGO = {id_jogo}"))[0]
+            registro_escola = self.validar_escola(registro_jogo["cnpj"])
+
+            escolha = input(f"Deseja excluir o jogo de ID {id_jogo}? [sim/nao]: ").lower()[0]
+
+            if escolha == "s":
+                print("[!] Caso existam times cadastrados no jogo, os mesmos serão retirados, mas não deletados.")
+                escolha = input(f"Confirma a exclusão do jogo de ID {id_jogo}? [sim/nao]: ").lower()[0]
+
+                if escolha == "s":
+                    if bool(json.loads(oracle.sqlToJson(f"SELECT ID_TIME FROM TIMES WHERE ID_JOGO = '{id_jogo}'"))):
+                        oracle.write(f"UPDATE TIMES SET ID_JOGO = NULL WHERE ID_JOGO = '{id_jogo}'")
+                        print("[-] Times retirados do jogo.")
+
+                    oracle.write(f"DELETE FROM JOGOS WHERE ID_JOGO = '{id_jogo}'")
+
+                    jogo = Jogo(id_jogo, registro_jogo["data_hora"], registro_escola)
+
+                    print("[-] Jogo removido.")
+                    print(jogo.to_string())
+                    return jogo
+        else:
+            print("[!] Jogo não cadastrado no sistema.")
+            return None
+
+    def verificar_registro(self, id_jogo:int) -> bool:
+        oracle = OracleQueries()
+        oracle.connect()
+        resultado = json.loads(oracle.sqlToJson(f"SELECT * FROM JOGOS WHERE ID_JOGO = {id_jogo}"))[0]
+        return bool(resultado)
 
     def validar_escola(self, cnpj: str) -> Union[Escola, None]:
         oracle = OracleQueries()
@@ -62,6 +123,12 @@ class ControllerJogo:
         else:
             print("[!] Escola não cadastrada no sistema.")
             return None
+       
+    def listar_jogos(self) -> None:
+        oracle = OracleQueries(can_write=True)
+        oracle.connect()
+        resultado = oracle.sqlToDataFrame("SELECT J.ID_JOGO, J.DATA_HORA, E.NOME AS NOME_ESCOLA FROM JOGOS J INNER JOIN ESCOLAS E ON J.CNPJ = E.CNPJ")
+        print(resultado)
 
     def listar_escolas(self) -> None:
         oracle = OracleQueries(can_write=True)
--- src/controller/controller_turma.py
+++ src/controller/controller_turma.py
@@ -1,3 +1,122 @@
+from model.turmas import Turma
+from model.escolas import Escola
+from controller.controller_escola import ControllerEscola
+from conexion.oracle_queries import OracleQueries
+from typing import Union
+
+import json
+
 class ControllerTurma:
     def __init__(self):
-        pass
+        self.controller_escola = ControllerEscola()
+
+    def inserir_registro(self) -> Union[Turma, None]:
+        self.listar_escolas()
+        cnpj_escola = input("Insira o CNPJ da escola desejada: ")
+        registro_escola = self.validar_escola(cnpj_escola)
+
+        if registro_escola is None:
+            return None
+
+        oracle = OracleQueries(can_write=True)
+        cursor = oracle.connect()
+
+        ano_turma = input("Insira o ano da turma (ano+letra): ").strip()
+        quantidade_alunos = input("Insira a quantidade de alunos: ").strip()
+        tipo_dado_id = cursor.var(int)
+
+        mapeamento_turma = dict(id_turma=tipo_dado_id, ano=ano_turma, quantidade_alunos=quantidade_alunos, cnpj=registro_escola.get_cnpj())
+
+        cursor.execute("""
+        BEGIN
+            :id_turma := TURMAS_ID_SEQ.NEXTVAL;
+            INSERT INTO TURMAS VALUES (:id_turma, :ano, :quantidade_alunos, :cnpj);
+        END;
+        """, mapeamento_turma)
+
+        id_turma = tipo_dado_id.getvalue()
+        oracle.conn.commit()
+
+        registro_turma = json.loads(oracle.sqlToJson(F"SELECT ID_TURMA, ANO, QUANTIDADE_ALUNOS FROM TURMAS WHERE ID_TURMA = '{id_turma}'"))[0]
+        turma = Turma(registro_turma["id_turma"], registro_turma["ano"], registro_turma["quantidade_alunos"], registro_escola)
+        print("[+]", turma.to_string())
+        return turma
+   
+    def atualizar_registro(self) -> Union[Turma, None]:
+        id_turma = int(input("ID da turma que deseja alterar: "))
+
+        if self.verificar_registro(id_turma):
+            nova_quantidade_alunos = int(input("Insira a nova quantidade de alunos: "))
+            oracle = OracleQueries(can_write=True)
+            oracle.connect()
+            oracle.write(f"UPDATE TURMAS SET QUANTIDADE_ALUNOS = '{nova_quantidade_alunos}' WHERE ID_TURMA = '{id_turma}'")
+
+            registro_turma = json.loads(oracle.sqlToJson(f"SELECT ID_TURMA, ANO, QUANTIDADE_ALUNOS, CNPJ FROM TURMAS WHERE ID_TURMA = '{id_turma}'"))[0]
+            registro_escola = json.loads(oracle.sqlToJson(f"SELECT * FROM ESCOLAS WHERE CNPJ = {registro_turma['cnpj']}"))[0]
+
+            escola = Escola(registro_escola["cnpj"], registro_escola["nome"], registro_escola["nivel_ensino"], registro_escola["endereco"], registro_escola["telefone"])
+            turma = Turma(registro_turma["id_turma"], registro_turma["ano"], registro_turma["quantidade_alunos"], escola)
+
+            print("[^+]", turma.to_string())
+            return turma
+        else:
+            print("[!] A turma não existe no sistema.")
+            return None
+
+    def deletar_registro(self) -> Union[Turma, None]:
+        id_turma = int(input("ID da turma que deseja deletar: "))
+
+        if self.verificar_registro(id_turma):
+            oracle = OracleQueries(can_write=True)
+            oracle.connect()
+            registro_turma = json.loads(oracle.sqlToJson(f"SELECT * FROM TURMAS WHERE ID_TURMA = '{id_turma}'"))[0]
+            escolha = input("Confirma que deseja deletar a turma? [sim/nao]: ").lower()[0]
+
+            if escolha == "s":
+                escolha = input("Todos os jogadores e times da turma serão deletados. Confirma a exclusão? [sim/nao]: ").lower()[0]
+
+                if escolha == "s":
+                    id_time = json.loads(oracle.sqlToJson(f"SELECT ID_TIME FROM TIMES WHERE ID_TURMA = '{id_turma}'"))[0]["id_time"]
+                    cpfs_jogadores = json.loads(oracle.sqlToJson(f"SELECT CPF FROM JOGADORES WHERE ID_TIME = '{id_time}'"))
+
+                    if cpfs_jogadores:
+                        for cpf in cpfs_jogadores:
+                            oracle.write(f"DELETE FROM JOGADORES WHERE CPF = '{cpf['cpf']}'")
+                       
+                        print("[-] Jogadores removidos.")
+
+                    oracle.write(f"DELETE FROM TIMES WHERE ID_TIME = '{id_time}'")
+                    print("[-] Time removidos.")
+
+                    oracle.write(f"DELETE FROM TURMAS WHERE ID_TURMA = {id_turma}")
+
+                    registro_escola = json.loads(oracle.sqlToJson(f"SELECT * FROM ESCOLAS WHERE CNPJ = '{registro_turma['cnpj']}'"))[0]
+                    escola = Escola(registro_escola["cnpj"], registro_escola["nome"], registro_escola["nivel_ensino"], registro_escola["endereco"], registro_escola["telefone"])
+                    turma = Turma(registro_turma["id_turma"], registro_turma["ano"], registro_turma["quantidade_alunos"], escola)
+
+                    print("[-]", turma.to_string())
+                    return turma
+        else:
+            print("[!] Turma não existente no sistema.")
+            return None
+
+    def listar_escolas(self):
+        oracle = OracleQueries()
+        oracle.connect()
+        resultado = oracle.sqlToDataFrame("SELECT CNPJ, NOME, NIVEL_ENSINO FROM ESCOLAS")
+        print(resultado)
+
+    def validar_escola(self, cnpj:str):
+        if self.controller_escola.verificar_registro(cnpj):
+            oracle = OracleQueries(can_write=True)
+            oracle.connect()
+            registro_escola = json.loads(oracle.sqlToJson(f"SELECT * FROM ESCOLAS WHERE CNPJ = {cnpj}"))[0]
+            return Escola(registro_escola["cnpj"], registro_escola["nome"], registro_escola["nivel_ensino"], registro_escola["endereco"], registro_escola["telefone"])
+        else:
+            return None
+   
+    def verificar_registro(self, id_turma:int):
+        oracle = OracleQueries(can_write=True)
+        oracle.connect()
+        resultado = json.loads(oracle.sqlToJson(f"SELECT ID_TURMA FROM TURMAS WHERE ID_TURMA = {id_turma}"))
+        return bool(resultado)
